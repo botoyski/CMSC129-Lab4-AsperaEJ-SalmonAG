a1c78ccac8f5d28870f92262aba5c3549da49a81
Eryl Joseph Aspera <eaaspera1@up.edu.ph>
2026-05-17 01:51:14 +0800

    [GREEN] Implement Task API integration (POST /tasks, GET /tasks)

--- server/app.js
+++ server/app.js
@@ -1,4 +1,24 @@
 const express = require("express");
+
 const app = express();
 app.use(express.json());
+
+let tasks = [];
+
+app.post("/tasks", (req, res) => {
+  const task = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    completed: false,
+  };
+
+  tasks.push(task);
+
+  res.status(201).json(task);
+});
+
+app.get("/tasks", (req, res) => {
+  res.status(200).json(tasks);
+});
+
 module.exports = app;
